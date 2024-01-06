1fa1c20be4fddee5bbd1f7e04ec74f2a0207ba63
J3SUNG <wptjd6141@naver.com>
2024-01-06 23:21:18 +0900

    FEPIT-8 : 파일 위치 이동, 웹팩 코드 수정

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "src"),
+    },
+    compress: true,
+    port: 9000,
+  },
+};
