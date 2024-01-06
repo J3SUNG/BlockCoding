07b5eacbb33ffc6f5ff8454a1b194b9478fbe6b1
J3SUNG <wptjd6141@naver.com>
2024-01-06 23:34:08 +0900

    FEPIT-8 : eslint 설정

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    jest: true
+  },
+  extends: [
+    '@kos/styleguide/comment',
+    '@kos/styleguide/es3',
+    '@kos/styleguide/es2015',
+    '@kos/styleguide/typescript'
+  ],
+  parserOptions: {
+    project: [
+      './tsconfig.json'
+    ],
+    sourceType: 'module'
+  }
+}
