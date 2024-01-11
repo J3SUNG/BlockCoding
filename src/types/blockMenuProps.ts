31c1b584e7bbebedfac6d1f63456b9e03aca8476
J3SUNG <wptjd6141@naver.com>
2024-01-11 17:56:23 +0900

    FEPIT-77 : blockMenuProps 타입 코드 수정

--- src/types/blockMenuProps.ts
+++ src/types/blockMenuProps.ts
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 import { BlockList, SelectedBlock, SetBlockList, SetSelectedBlock } from './stateType';
-=======
-type SelectedBlock = number;
-type SetSelectedBlock = (selectedBlock: SelectedBlock) => void;
-type BlockList = any[];
-type SetBlockList = (blockList: BlockList) => void;
->>>>>>> parent of 30c6a77 (FEPIT-77 : 대소문자 변경)
 
 export interface blockMenuProps {
   selectedBlock: SelectedBlock;
