9c70ac0f72d5ec0e4df6f3c68f17d468d4ccaab7
J3SUNG <wptjd6141@naver.com>
2024-01-12 11:18:40 +0900

    FEPIT-86 : paintDiagram 코드 정리

--- src/utils/paintDiagram.ts
+++ src/utils/paintDiagram.ts
@@ -3,33 +3,36 @@ import { PaintDiagramProps } from '../types/paintDiagramProps';
 export const paintDiagram = ({ x, y, width, height, type }: PaintDiagramProps) => {
   let d = '';
   let path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+  let color = '';
 
   if (type === 'declare') {
     d = `M ${x} ${y} L ${x + width - height * 0.35} ${y} L ${x + width} ${y + height * 0.5} L ${
       x + width - height * 0.35
     } ${y + height} L ${x} ${y + height} Z`;
-    path.setAttribute('fill', 'green');
+    color = 'green';
   } else if (type === 'general') {
     d = `M ${x} ${y} L ${x + width} ${y} L ${x + width} ${y + height} L ${x} ${y + height} Z`;
-    path.setAttribute('fill', 'orange');
+    color = 'orange';
   } else if (type === 'control') {
     d = `M ${x} ${y} L ${x + width} ${y} L ${x + width} ${y + height * 0.35} L ${x + height * 0.35} ${
       y + height * 0.35
     } L ${x + height * 0.35} ${y + height * 0.65} L ${x + width} ${y + height * 0.65} L ${x + width} ${
       y + height
     } L ${x} ${y + height} Z`;
-    path.setAttribute('fill', 'skyblue');
+    color = 'skyblue';
   } else if (type === 'expressionValue') {
     d = `M ${x + height * 0.35} ${y} L ${x + width - height * 0.35} ${y} L ${x + width} ${y + height * 0.5} L ${
       x + width - height * 0.35
     } ${y + height} L ${x + height * 0.35} ${y + height} L ${x} ${y + height * 0.5} Z`;
-    path.setAttribute('fill', 'yellow');
+    color = 'yellow';
   } else if (type === 'expressionLogical') {
     d = `M ${x + width * 0.5} ${y} A ${width * 0.5} ${height * 0.5} 0 0 1 ${x + width * 0.5} ${y + height} A ${
       width * 0.5
     } ${height * 0.5} 0 0 1 ${x + width * 0.5} ${y} Z`;
-    path.setAttribute('fill', 'blue');
+    color = 'blue';
   }
+
+  path.setAttribute('fill', color);
   path.setAttribute('d', d);
   path.setAttribute('stroke', 'black');
 
