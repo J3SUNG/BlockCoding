d81e92e700cdfdc6fd92c46e0f9a38d4e8500657
J3SUNG <wptjd6141@naver.com>
2024-01-12 10:39:28 +0900

    FEPIT-86 : 그리기 함수 생성

--- src/components/workspace/workspace.ts
+++ src/components/workspace/workspace.ts
@@ -1,3 +1,4 @@
+import { paintDiagram } from '../../utils/paintDiagram';
 import { WorkspaceProps } from '../../types/workspaceProps';
 import { createElementCommon } from '../../utils/createElementCommon';
 import { workspaceArea } from './workspaceArea';
@@ -8,6 +9,22 @@ export const workspace = ({ blockList }: WorkspaceProps) => {
   const trashIcon = createElementCommon('span', { className: 'material-symbols-outlined', textContent: 'delete' });
   const workspaceAreaElement = workspaceArea({ blockList });
 
+  let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  svg.setAttribute('width', 'full');
+  svg.setAttribute('height', 'full');
+
+  let x = 20;
+  let y = 20;
+  let width = 100;
+  let height = 60;
+
+  blockList.forEach((block) => {
+    const path = paintDiagram({ x, y, width, height, type: block.type });
+    svg.appendChild(path);
+    y += height;
+  });
+
+  section.appendChild(svg);
   trashBin.appendChild(trashIcon);
   section.appendChild(workspaceAreaElement);
   section.appendChild(trashBin);
