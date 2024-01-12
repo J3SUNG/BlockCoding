3214e0fc6a52a5a7e870bff9270b6c51f4b285ec
J3SUNG <wptjd6141@naver.com>
2024-01-12 13:37:11 +0900

    FEPIT-86 : path 변수 const로 할당, color 배열로 수정

--- src/utils/paintDiagram.ts
+++ src/utils/paintDiagram.ts
@@ -2,37 +2,38 @@ import { PaintDiagramProps } from '../types/paintDiagramProps';
 
 export const paintDiagram = ({ x, y, width, height, type }: PaintDiagramProps) => {
   let d = '';
-  let path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-  let color = '';
+  let colorIndex = 0;
+  const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+  const color = ['tomato', 'orange', 'dodgerblue', 'mediumseagreen', 'violet'];
 
   if (type === 'declare') {
     d = `M ${x} ${y} L ${x + width - height * 0.35} ${y} L ${x + width} ${y + height * 0.5} L ${
       x + width - height * 0.35
     } ${y + height} L ${x} ${y + height} Z`;
-    color = 'green';
+    colorIndex = 0;
   } else if (type === 'general') {
     d = `M ${x} ${y} L ${x + width} ${y} L ${x + width} ${y + height} L ${x} ${y + height} Z`;
-    color = 'orange';
+    colorIndex = 1;
   } else if (type === 'control') {
     d = `M ${x} ${y} L ${x + width} ${y} L ${x + width} ${y + height * 0.35} L ${x + height * 0.35} ${
       y + height * 0.35
     } L ${x + height * 0.35} ${y + height * 0.65} L ${x + width} ${y + height * 0.65} L ${x + width} ${
       y + height
     } L ${x} ${y + height} Z`;
-    color = 'skyblue';
+    colorIndex = 2;
   } else if (type === 'expressionValue') {
     d = `M ${x + height * 0.35} ${y} L ${x + width - height * 0.35} ${y} L ${x + width} ${y + height * 0.5} L ${
       x + width - height * 0.35
     } ${y + height} L ${x + height * 0.35} ${y + height} L ${x} ${y + height * 0.5} Z`;
-    color = 'yellow';
+    colorIndex = 3;
   } else if (type === 'expressionLogical') {
     d = `M ${x + width * 0.5} ${y} A ${width * 0.5} ${height * 0.5} 0 0 1 ${x + width * 0.5} ${y + height} A ${
       width * 0.5
     } ${height * 0.5} 0 0 1 ${x + width * 0.5} ${y} Z`;
-    color = 'blue';
+    colorIndex = 4;
   }
 
-  path.setAttribute('fill', color);
+  path.setAttribute('fill', color[colorIndex]);
   path.setAttribute('d', d);
   path.setAttribute('stroke', 'black');
 
