4e50e56972df6df539c0530358388f1d8119ceca
J3SUNG <wptjd6141@naver.com>
2024-01-15 14:20:29 +0900

    FEPIT-88 : 배포 오류 수정

--- src/components/workspace/workspace.ts
+++ src/components/workspace/workspace.ts
@@ -1,8 +1,6 @@
-import { BlockInputObj, BlockInputProps } from '../../types/blockInputObj';
-import { useState } from '../../core/core';
 import { BlockList } from '../../types/stateType';
 import { createElementCommon } from '../../utils/createElementCommon';
-import { blockInput } from '../block/blockInput';
+
 export const workspace = ({ blockList }: { blockList: BlockList }) => {
   const section = createElementCommon('section', { id: 'workspace' });
   const trashBin = createElementCommon('div', { id: 'trash-bin' });
