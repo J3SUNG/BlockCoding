6c56b7c38e6fbcb2bd116cca6cf5e6f900cf03ad
J3SUNG <wptjd6141@naver.com>
2024-01-15 14:44:13 +0900

    FEPIT-97 : 출력 블럭 그리기 구현

--- src/components/block/blockController.ts
+++ src/components/block/blockController.ts
@@ -1,5 +1,6 @@
 import { BlockPaintProps } from '@/types/blockPaintProps';
 import { blockStart } from './blockStart';
+import { blockOutput } from './blockOutput';
 
 interface BlockControllerProps extends BlockPaintProps {
   setBlockInputObj: ({ x, y, value, isView, setBlockValue }: any) => void;
@@ -8,5 +9,7 @@ interface BlockControllerProps extends BlockPaintProps {
 export const blockController = ({ x, y, width, height, name, setBlockInputObj }: BlockControllerProps) => {
   if (name === 'start') {
     return blockStart({ x, y, width: 220, height, name });
+  } else if (name === 'output') {
+    return blockOutput({ x, y, width: 105, height, name });
   }
 };
