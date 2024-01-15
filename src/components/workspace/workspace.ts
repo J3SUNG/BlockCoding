babf48601386c6273665d67da29be88b8266ed47
J3SUNG <wptjd6141@naver.com>
2024-01-15 12:59:58 +0900

    FEPIT-88 : SVG 코드 재작성을 위해 제거

--- src/components/workspace/workspace.ts
+++ src/components/workspace/workspace.ts
@@ -1,35 +1,14 @@
-import { paintDiagram } from '../../utils/paintDiagram';
-import { WorkspaceProps } from '../../types/workspaceProps';
+import { BlockInputObj, BlockInputProps } from '../../types/blockInputObj';
+import { useState } from '../../core/core';
+import { BlockList } from '../../types/stateType';
 import { createElementCommon } from '../../utils/createElementCommon';
-import { workspaceArea } from './workspaceArea';
-import { paintText } from '../../utils/paintText';
-
-export const workspace = ({ blockList }: WorkspaceProps) => {
+import { blockInput } from '../block/blockInput';
+export const workspace = ({ blockList }: { blockList: BlockList }) => {
   const section = createElementCommon('section', { id: 'workspace' });
   const trashBin = createElementCommon('div', { id: 'trash-bin' });
   const trashIcon = createElementCommon('span', { className: 'material-symbols-outlined', textContent: 'delete' });
-  const workspaceAreaElement = workspaceArea({ blockList });
 
-  let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-  svg.setAttribute('width', '100%');
-  svg.setAttribute('height', '100%');
-
-  let x = 20;
-  let y = 20;
-  let width = 100;
-  let height = 60;
-
-  blockList.forEach((block) => {
-    const path = paintDiagram({ x, y, width, height, type: block.type });
-    const text = paintText({ x, y, name: block.korName });
-    svg.appendChild(path);
-    svg.appendChild(text);
-    y += height;
-  });
-
-  section.appendChild(svg);
   trashBin.appendChild(trashIcon);
-  section.appendChild(workspaceAreaElement);
   section.appendChild(trashBin);
 
   return section;
