7dc1de605b4a985cf3f4360393f29e1afebf85f6
J3SUNG <wptjd6141@naver.com>
2024-01-16 17:46:15 +0900

    FEPIT-101 : objParser 함수 분리

--- src/components/workspace/workspaceSvg.ts
+++ src/components/workspace/workspaceSvg.ts
@@ -1,12 +1,22 @@
 import { BlockList, SetBlockList } from '../../types/stateType';
 import { blockController } from '../block/blockController';
 import { BlockInputObj } from '../../types/blockInputObj';
+import { test } from '../../utils/test';
 // import { makeDraggable } from '../../utils/makeDraggable';
 
 interface WorkspaceSvgProps {
   blockList: BlockList;
   setBlockList: SetBlockList;
   setBlockInputObj: ({ x, y, value, isView, setBlockValue }: BlockInputObj) => void;
+}
+
+interface objParserProps {
+  svg: any;
+  obj: any;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
 }
 
 export const workspaceSvg = ({ blockList, setBlockList, setBlockInputObj }: WorkspaceSvgProps) => {
@@ -19,37 +29,38 @@ export const workspaceSvg = ({ blockList, setBlockList, setBlockInputObj }: Work
   svg.setAttribute('width', '100%');
   svg.setAttribute('height', '100%');
 
-  function objParser(obj: any, x: number, y: number, width: number, height: number) {
-    if (!obj) return;
-
-    if (Array.isArray(obj)) {
-      let originalY = y;
-      for (let item of obj) {
-        objParser(item, x, y, width, height);
-        y += height;
-      }
-      y = originalY;
-    } else {
-      if (obj.data && obj.data.value) {
-        const g = blockController({
-          x: obj.data.x,
-          y: obj.data.y,
-          width,
-          height,
-          name: obj.name,
-          value: obj.data.value.toString(),
-          id: obj.data.id,
-        });
-        svg.appendChild(g!);
-
-        objParser(obj.data.value + 1, x, y, width, height);
-      }
-    }
-  }
-
-  objParser(blockList, x, y, width, height);
+  objParser({ svg, obj: test, x, y, width, height });
+  // objParser(blockList, x, y, width, height);
 
   // makeDraggable({ svg, blockList, setBlockList });
 
   return svg;
+};
+
+const objParser = ({ svg, obj, x, y, width, height }: objParserProps) => {
+  if (!obj) return;
+
+  if (Array.isArray(obj)) {
+    let originalY = y;
+    for (let item of obj) {
+      objParser({ svg, obj: item, x, y, width, height });
+      y += height;
+    }
+    y = originalY;
+  } else {
+    if (obj.data && obj.data.value) {
+      const g = blockController({
+        x: obj.data.x,
+        y: obj.data.y,
+        width,
+        height,
+        name: obj.name,
+        value: obj.data.value.toString(),
+        id: obj.data.id,
+      });
+      svg.appendChild(g);
+
+      objParser({ svg, obj: obj.data.value, x, y, width, height });
+    }
+  }
 };
