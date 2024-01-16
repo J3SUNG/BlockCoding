f45b6c52bcbc1da75e09248e0fe143f23a86d0fa
J3SUNG <wptjd6141@naver.com>
2024-01-16 17:41:41 +0900

    FEPIT-101 : gnb 시작 이벤트 추가

--- src/components/gnb/gnb.ts
+++ src/components/gnb/gnb.ts
@@ -1,6 +1,12 @@
+import { PROGRAM_RUN } from '../../constants/programState';
+import { SetProgramState } from '../../types/stateType';
 import { createElementCommon } from '../../utils/createElementCommon';
 
-export const gnb = () => {
+interface GnbProps {
+  setProgramState: SetProgramState;
+}
+
+export const gnb = ({ setProgramState }: GnbProps) => {
   const header = createElementCommon('header', { id: 'gnb' });
   const h1 = createElementCommon('h1', { id: 'title', textContent: 'Block Coding' });
   const nav = createElementCommon('nav', {});
@@ -8,6 +14,10 @@ export const gnb = () => {
   const loadButton = createElementCommon('button', { type: 'button', className: 'bg-gray', textContent: 'Load' });
   const playButton = createElementCommon('button', { type: 'button', className: 'bg-green', textContent: '▶' });
   const stopButton = createElementCommon('button', { type: 'button', className: 'bg-red', textContent: '⏹' });
+
+  playButton.addEventListener('click', () => {
+    setProgramState(PROGRAM_RUN);
+  });
 
   nav.appendChild(saveButton);
   nav.appendChild(loadButton);
--- src/pages/blockCoding.ts
+++ src/pages/blockCoding.ts
@@ -1,9 +1,15 @@
+import { useState } from '../core/core';
 import { gnb } from '../components/gnb/gnb';
 import { main } from '../components/main/main';
+import { PROGRAM_STOP } from '../constants/programState';
+import { ConsoleLog, ProgramState } from '../types/stateType';
 
 export const blockCoding = () => {
-  const gnbComponent = gnb();
-  const mainComponent = main();
+  const [programState, setProgramState] = useState<ProgramState>(PROGRAM_STOP);
+  const [consoleLog, setConsoleLog] = useState<ConsoleLog>([]);
+
+  const gnbComponent = gnb({ setProgramState });
+  const mainComponent = main({ programState, consoleLog, setConsoleLog, setProgramState });
 
   const fragment = document.createDocumentFragment();
   fragment.appendChild(gnbComponent);
