6dbe4966e2c34551bd857193c2f4f0cc876da334
J3SUNG <wptjd6141@naver.com>
2024-01-17 18:55:33 +0900

    FEPIT-111 : 사용하지 않는 상태 값 제거

--- src/components/workspace/workspace.ts
+++ src/components/workspace/workspace.ts
@@ -1,9 +1,6 @@
-import { BlockInputObj } from '../../types/blockInputObj';
-import { useState } from '../../core/core';
 import { BlockList, SetBlockList } from '../../types/stateType';
 import { createElementCommon } from '../../utils/createElementCommon';
-import { blockInput } from '../block/blockInput';
-import { workspaceSvg } from './workspaceSvg';
+import { workspaceSection } from './workspaceSection';
 
 interface WorkspaceProps {
   blockList: BlockList;
@@ -11,19 +8,10 @@ interface WorkspaceProps {
 }
 
 export const workspace = ({ blockList, setBlockList }: WorkspaceProps) => {
-  const section = createElementCommon('section', { id: 'workspace' });
+  const section = workspaceSection({ blockList, setBlockList });
   const trashBin = createElementCommon('div', { id: 'trash-bin' });
   const trashIcon = createElementCommon('span', { className: 'material-symbols-outlined', textContent: 'delete' });
-  const [blockInputObj, setBlockInputObj] = useState<BlockInputObj>({
-    x: undefined,
-    y: undefined,
-    value: undefined,
-    isView: false,
-    setBlockValue: undefined,
-  });
-  const svg = workspaceSvg({ blockList, setBlockList, setBlockInputObj });
 
-  section.appendChild(svg);
   trashBin.appendChild(trashIcon);
   section.appendChild(trashBin);
 
