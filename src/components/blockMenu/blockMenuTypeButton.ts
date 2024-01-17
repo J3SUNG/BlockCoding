250153c902c194f0ea4488ee71d13808c0a8988f
J3SUNG <wptjd6141@naver.com>
2024-01-17 18:54:46 +0900

    FEPIT-111 : 블럭메뉴 타입버튼 svg -> div로 변경

---
+++ src/components/blockMenu/blockMenuTypeButton.ts
@@ -0,0 +1,81 @@
+import { createElementCommon } from '../../utils/createElementCommon';
+
+interface BlockMenuTypeButtonProps {
+  type: string;
+  korName: string;
+}
+
+interface BlockTypePaintProps {
+  korName: string;
+}
+
+export const blockMenuTypeButton = ({ type, korName }: BlockMenuTypeButtonProps) => {
+  if (type === 'declare') {
+    return blockTypeDeclare({ korName });
+  } else if (type === 'general') {
+    return blockTypeGeneral({ korName });
+  } else if (type === 'control') {
+    return blockTypeControl({ korName });
+  } else if (type === 'expressionValue') {
+    return blockTypeExpressionValue({ korName });
+  } else if (type === 'expressionLogical') {
+    return blockTypeExpressionLogical({ korName });
+  } else {
+    throw new Error('타입메뉴 버튼 생성 오류 : 정의 되지 않은 타입입니다.');
+  }
+};
+
+const blockTypeDeclare = ({ korName }: BlockTypePaintProps) => {
+  const li = createElementCommon('li', { className: 'nav-type__button nav-type__button--declare' });
+  const div = createElementCommon('div', { className: 'block' });
+  const p = createElementCommon('p', { className: 'nav-type__button-text', textContent: korName });
+
+  div.appendChild(p);
+  li.appendChild(div);
+
+  return li;
+};
+
+const blockTypeGeneral = ({ korName }: BlockTypePaintProps) => {
+  const li = createElementCommon('li', { className: 'nav-type__button nav-type__button--general' });
+  const div = createElementCommon('div', { className: 'block' });
+  const p = createElementCommon('p', { className: 'nav-type__button-text', textContent: korName });
+
+  div.appendChild(p);
+  li.appendChild(div);
+
+  return li;
+};
+
+const blockTypeControl = ({ korName }: BlockTypePaintProps) => {
+  const li = createElementCommon('li', { className: 'nav-type__button nav-type__button--control' });
+  const div = createElementCommon('div', { className: 'block' });
+  const p = createElementCommon('p', { className: 'nav-type__button-text', textContent: korName });
+
+  div.appendChild(p);
+  li.appendChild(div);
+
+  return li;
+};
+
+const blockTypeExpressionValue = ({ korName }: BlockTypePaintProps) => {
+  const li = createElementCommon('li', { className: 'nav-type__button nav-type__button--expression-value' });
+  const div = createElementCommon('div', { className: 'block' });
+  const p = createElementCommon('p', { className: 'nav-type__button-text', textContent: korName });
+
+  div.appendChild(p);
+  li.appendChild(div);
+
+  return li;
+};
+
+const blockTypeExpressionLogical = ({ korName }: BlockTypePaintProps) => {
+  const li = createElementCommon('li', { className: 'nav-type__button nav-type__button--expression-logical' });
+  const div = createElementCommon('div', { className: 'block' });
+  const p = createElementCommon('p', { className: 'nav-type__button-text', textContent: korName });
+
+  div.appendChild(p);
+  li.appendChild(div);
+
+  return li;
+};
--- src/components/blockMenu/blockMenuTypeNav.ts
+++ src/components/blockMenu/blockMenuTypeNav.ts
@@ -1,7 +1,7 @@
 import { BLOCK_TYPE_OBJECT } from '../../constants/blockTypeObject';
 import { createElementCommon } from '../../utils/createElementCommon';
 import { SelectedType, SetSelectedType, SetSelectedTypeBlock } from '../../types/stateType';
-import { blockType } from '../block/blockType';
+import { blockMenuTypeButton } from './blockMenuTypeButton';
 
 interface BlockMenuTypeNavProps {
   selectedType: SelectedType;
@@ -11,32 +11,22 @@ interface BlockMenuTypeNavProps {
 
 export const blockMenuTypeNav = ({ selectedType, setSelectedType, setSelectedTypeBlock }: BlockMenuTypeNavProps) => {
   const blockTypeNav = createElementCommon('nav', { id: 'nav-type' });
-  const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-  svg.setAttribute('width', '100%');
-  svg.setAttribute('height', '100%');
-
-  const typeBlockListG = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+  const ul = createElementCommon('ul', {});
 
   BLOCK_TYPE_OBJECT.forEach((block, index) => {
-    const typeBlockG = blockType({
-      x: 15,
-      y: 80 * index,
-      width: 100,
-      height: 60,
+    const li = blockMenuTypeButton({
       type: block.name,
       korName: block.korName,
     });
 
-    typeBlockG.addEventListener('click', () => {
+    li.addEventListener('click', () => {
       setSelectedType(index);
       setSelectedTypeBlock(-1);
     });
 
-    typeBlockListG.appendChild(typeBlockG);
+    ul.appendChild(li);
   });
-
-  svg.appendChild(typeBlockListG);
-  blockTypeNav.appendChild(svg);
+  blockTypeNav.appendChild(ul);
 
   return blockTypeNav;
 };
