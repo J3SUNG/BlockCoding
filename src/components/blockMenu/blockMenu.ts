b5c0f4d81a31d8fa06e88edc6cc63c21de7ea5ce
J3SUNG <wptjd6141@naver.com>
2024-01-18 09:36:47 +0900

    FEPIT-111 : 타입 수정

--- src/components/blockMenu/blockMenu.ts
+++ src/components/blockMenu/blockMenu.ts
@@ -1,31 +1,14 @@
-import { BlockMenuProps } from '../../types/blockMenu';
 import { useState } from '../../core/core';
 import { createElementCommon } from '../../utils/createElementCommon';
 import { blockMenuBlockNav } from './blockMenuBlockNav';
 import { blockMenuTypeNav } from './blockMenuTypeNav';
 
-export const blockMenu = ({
-  selectedMenuBlock,
-  setSelectedMenuBlock,
-  blockList,
-  setBlockList,
-  uniqueId,
-  setUniqueId,
-}: BlockMenuProps) => {
+export const blockMenu = () => {
   const [selectedType, setSelectedType] = useState(0);
-  const [selectedTypeBlock, setSelectedTypeBlock] = useState(-1);
-
   const blockMenuDiv = createElementCommon('div', { id: 'block-menu' });
-  const blockTypeNav = blockMenuTypeNav({ selectedType, setSelectedType, setSelectedTypeBlock });
+  const blockTypeNav = blockMenuTypeNav({ setSelectedType });
   const blockNav = blockMenuBlockNav({
     selectedType,
-    selectedMenuBlock,
-    setSelectedMenuBlock,
-    setSelectedTypeBlock,
-    blockList,
-    setBlockList,
-    uniqueId,
-    setUniqueId,
   });
 
   blockMenuDiv.appendChild(blockTypeNav);
