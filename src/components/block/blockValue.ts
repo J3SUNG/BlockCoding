84e58bd6310d159dee729087182e93b2e5726ded
J3SUNG <wptjd6141@naver.com>
2024-01-19 12:31:30 +0900

    FEPIT-135 : value블럭의 인풋 값을 받아서 값을 바꾸는 기능 구현

--- src/components/block/blockValue.ts
+++ src/components/block/blockValue.ts
@@ -1,14 +1,59 @@
+import { camelToKebab } from '../../utils/camelToKebab';
+import { BlockCommonProps } from '../../types/blockCommonProps';
 import { createElementCommon } from '../../utils/createElementCommon';
+import { BlockObject } from '../../types/blockObject';
 
-interface BlockValueProps {
-  id: string;
+interface UpdateBlockValueProps {
+  targetUniqueId: string;
+  obj: BlockObject | BlockObject[] | string;
 }
 
-export const blockValue = ({ id }: BlockValueProps) => {
-  const div = createElementCommon('div', { id });
-  const p = createElementCommon('p', { className: 'block__text', textContent: '값' });
+export const blockValue = ({ id, x, y, type, value, blockList, setBlockList }: BlockCommonProps) => {
+  const div = createElementCommon('div', { id, className: `block block--${camelToKebab({ str: type })}` });
+  const input = createElementCommon('input', { className: 'block__input', value });
 
-  div.appendChild(p);
+  input.addEventListener('blur', (e: any) => {
+    const target = e.target as HTMLInputElement;
+    const { value } = target;
+
+    const div = target.closest('div');
+
+    if (setBlockList && blockList) {
+      const blockValueTarget = updateBlockValue({
+        targetUniqueId: div?.id ?? '',
+        obj: blockList,
+      });
+
+      if (blockValueTarget) {
+        blockValueTarget.data.value = value;
+        setBlockList([...blockList]);
+      }
+    }
+  });
+
+  div.setAttribute('style', `left: ${x}px; top: ${y}px`);
+  div.appendChild(input);
 
   return div;
+};
+
+const updateBlockValue = ({ targetUniqueId, obj }: UpdateBlockValueProps): BlockObject | null => {
+  if (!obj || (typeof obj === 'object' && Object.keys(obj).length === 0)) {
+    return null;
+  }
+
+  if (Array.isArray(obj)) {
+    for (const block of obj) {
+      const result: BlockObject | null = updateBlockValue({ targetUniqueId, obj: block });
+      if (result) return result; // 매칭되는 객체를 찾으면 반환하고 루프 중단
+    }
+  } else if (typeof obj === 'object' && obj !== null) {
+    if (obj.data.id === targetUniqueId) {
+      return obj; // 매칭되는 객체 반환
+    } else {
+      return updateBlockValue({ targetUniqueId, obj: obj.data.value });
+    }
+  }
+
+  return null;
 };
