193725e77149e1a08c982aa91ba83bd13b0603f5
J3SUNG <wptjd6141@naver.com>
2024-01-22 13:43:25 +0900

    FEPIT-157 :  constant object 배열형식에서 key로 접근 방식 변경

--- src/constants/blockObject.ts
+++ src/constants/blockObject.ts
@@ -1,140 +1,357 @@
 import { BlockObject } from '../types/blockObject';
 
-export const BLOCK_OBJECT: BlockObject[] = [
-  {
+interface ConstantBlockObject {
+  [key: string]: BlockObject;
+}
+
+const commponProps = {
+  id: '',
+  x: 20,
+  y: 20,
+};
+
+export const BLOCK_OBJECT: ConstantBlockObject = {
+  start: {
     name: 'start',
     type: 'declare',
     data: {
-      id: '',
-      x: 20,
-      y: 20,
-      value: [] as BlockObject[],
+      ...commponProps,
+      value: [],
     },
   },
-  {
+  variable: {
     name: 'variable',
     type: 'general',
     data: {
-      id: '',
-      x: 20,
-      y: 20,
+      ...commponProps,
       varName: {} as BlockObject,
       value: {} as BlockObject,
     },
   },
-  {
+  output: {
     name: 'output',
     type: 'general',
     data: {
-      id: '',
-      x: 20,
-      y: 20,
+      ...commponProps,
       value: {} as BlockObject,
     },
   },
-  {
+  timer: {
     name: 'timer',
     type: 'general',
     data: {
-      id: '',
-      x: 20,
-      y: 20,
+      ...commponProps,
       value: {} as BlockObject,
     },
   },
-  {
+  condition: {
     name: 'condition',
     type: 'control',
     data: {
-      id: '',
-      x: 20,
-      y: 20,
+      ...commponProps,
       condition: {} as BlockObject,
-      value: [] as BlockObject[],
+      value: [],
     },
   },
-  {
+  loop: {
     name: 'loop',
     type: 'control',
     data: {
-      id: '',
-      x: 20,
-      y: 20,
+      ...commponProps,
       condition: {} as BlockObject,
-      value: [] as BlockObject[],
+      value: [],
     },
   },
-  {
+  value: {
     name: 'value',
     type: 'expressionValue',
     data: {
-      id: '',
-      x: 20,
-      y: 20,
-      value: '10' as string,
+      ...commponProps,
+      value: '10',
     },
   },
-  {
+  input: {
     name: 'input',
     type: 'expressionValue',
     data: {
-      id: '',
-      x: 20,
-      y: 20,
+      ...commponProps,
       value: {} as BlockObject,
     },
   },
-  {
+  refVariable: {
     name: 'refVariable',
     type: 'expressionValue',
     data: {
-      id: '',
-      x: 20,
-      y: 20,
+      ...commponProps,
       value: {} as BlockObject,
     },
   },
-  {
+  arithmetic: {
     name: 'arithmetic',
     type: 'expressionValue',
     data: {
-      id: '',
-      x: 20,
-      y: 20,
-      operator: {} as BlockObject,
-      value: [] as BlockObject[],
+      ...commponProps,
+      operator: '',
+      value: [],
     },
   },
-  {
+  comparison: {
     name: 'comparison',
     type: 'expressionLogical',
     data: {
-      id: '',
-      x: 20,
-      y: 20,
-      operator: {} as BlockObject,
-      value: [] as BlockObject[],
+      ...commponProps,
+      operator: '',
+      value: [],
     },
   },
-  {
+  negation: {
     name: 'negation',
     type: 'expressionLogical',
     data: {
-      id: '',
-      x: 20,
-      y: 20,
+      ...commponProps,
       value: {} as BlockObject,
     },
   },
-  {
+  logical: {
     name: 'logical',
     type: 'expressionLogical',
     data: {
-      id: '',
-      x: 20,
-      y: 20,
-      operator: {} as BlockObject,
-      value: [] as BlockObject[],
+      ...commponProps,
+      operator: '',
+      value: [],
     },
   },
-];
+};
+
+// import { BlockObject } from 'types/blockObject';
+
+// interface BlockBaseProps {
+//   id: string;
+//   x: number;
+//   y: number;
+// }
+
+// interface BlockStartProps extends BlockBaseProps {
+//   value: BlockObject[];
+// }
+
+// interface BlockVariableProps extends BlockBaseProps {
+//   varName: BlockObject;
+//   value: BlockObject;
+// }
+
+// interface BlockOutputProps extends BlockBaseProps {
+//   value: BlockObject;
+// }
+
+// interface BlockTimerProps extends BlockBaseProps {
+//   value: BlockObject;
+// }
+
+// interface BlockConditionProps extends BlockBaseProps {
+//   condition: BlockObject;
+//   value: BlockObject[];
+// }
+
+// interface BlockLoopProps extends BlockBaseProps {
+//   condition: BlockObject;
+//   value: BlockObject[];
+// }
+
+// interface BlockValueProps extends BlockBaseProps {
+//   value: string;
+// }
+
+// interface BlockInputProps extends BlockBaseProps {
+//   value: BlockObject;
+// }
+
+// interface BlockRefVariableProps extends BlockBaseProps {
+//   value: BlockObject;
+// }
+
+// interface BlockArithmeticProps extends BlockBaseProps {
+//   operator: string;
+//   value: BlockObject[];
+// }
+
+// interface BlockComparisonProps extends BlockBaseProps {
+//   operator: string;
+//   value: BlockObject[];
+// }
+
+// interface BlockNegationProps extends BlockBaseProps {
+//   value: BlockObject;
+// }
+
+// interface BlockLogicalProps extends BlockBaseProps {
+//   operator: string;
+//   value: BlockObject[];
+// }
+
+// class BlockBase {
+//   id: string;
+//   x: number;
+//   y: number;
+
+//   constructor(id: string, x: number, y: number) {
+//     this.id = id;
+//     this.x = x;
+//     this.y = y;
+//   }
+// }
+
+// export class BlockStart extends BlockBase {
+//   name: string = 'start';
+//   type: string = 'declare';
+//   value: BlockObject[];
+
+//   constructor(props: BlockStartProps) {
+//     super(props.id, props.x, props.y);
+//     this.value = props.value;
+//   }
+// }
+
+// export class BlockVariable extends BlockBase {
+//   name: string = 'variable';
+//   type: string = 'general';
+//   varName: BlockObject;
+//   value: BlockObject;
+
+//   constructor(props: BlockVariableProps) {
+//     super(props.id, props.x, props.y);
+//     this.varName = props.varName;
+//     this.value = props.value;
+//   }
+// }
+
+// export class BlockOutput extends BlockBase {
+//   name: string = 'output';
+//   type: string = 'general';
+//   value: BlockObject;
+
+//   constructor(props: BlockOutputProps) {
+//     super(props.id, props.x, props.y);
+//     this.value = props.value;
+//   }
+// }
+
+// export class BlockTimer extends BlockBase {
+//   name: string = 'timer';
+//   type: string = 'general';
+//   value: BlockObject;
+
+//   constructor(props: BlockTimerProps) {
+//     super(props.id, props.x, props.y);
+//     this.value = props.value;
+//   }
+// }
+
+// export class BlockCondition extends BlockBase {
+//   name: string = 'condition';
+//   type: string = 'control';
+//   condition: BlockObject;
+//   value: BlockObject[];
+
+//   constructor(props: BlockConditionProps) {
+//     super(props.id, props.x, props.y);
+//     this.condition = props.condition;
+//     this.value = props.value;
+//   }
+// }
+
+// export class BlockLoop extends BlockBase {
+//   name: string = 'loop';
+//   type: string = 'control';
+//   condition: BlockObject;
+//   value: BlockObject[];
+
+//   constructor(props: BlockLoopProps) {
+//     super(props.id, props.x, props.y);
+//     this.condition = props.condition;
+//     this.value = props.value;
+//   }
+// }
+
+// export class BlockValue extends BlockBase {
+//   name: string = 'value';
+//   type: string = 'expressionValue';
+//   value: string;
+
+//   constructor(props: BlockValueProps) {
+//     super(props.id, props.x, props.y);
+//     this.value = props.value;
+//   }
+// }
+
+// export class BlockInput extends BlockBase {
+//   name: string = 'input';
+//   type: string = 'expressionValue';
+//   value: BlockObject;
+
+//   constructor(props: BlockInputProps) {
+//     super(props.id, props.x, props.y);
+//     this.value = props.value;
+//   }
+// }
+
+// export class BlockRefVariable extends BlockBase {
+//   name: string = 'refVariable';
+//   type: string = 'expressionValue';
+//   value: BlockObject;
+
+//   constructor(props: BlockRefVariableProps) {
+//     super(props.id, props.x, props.y);
+//     this.value = props.value;
+//   }
+// }
+
+// export class BlockArithmetic extends BlockBase {
+//   name: string = 'arithmetic';
+//   type: string = 'expressionValue';
+//   operator: string;
+//   value: BlockObject[];
+
+//   constructor(props: BlockArithmeticProps) {
+//     super(props.id, props.x, props.y);
+//     this.operator = props.operator;
+//     this.value = props.value;
+//   }
+// }
+
+// export class BlockComparison extends BlockBase {
+//   name: string = 'comparison';
+//   type: string = 'expressionLogical';
+//   operator: string;
+//   value: BlockObject[];
+
+//   constructor(props: BlockComparisonProps) {
+//     super(props.id, props.x, props.y);
+//     this.operator = props.operator;
+//     this.value = props.value;
+//   }
+// }
+
+// export class BlockNegation extends BlockBase {
+//   name: string = 'negation';
+//   type: string = 'expressionLogical';
+//   value: BlockObject;
+
+//   constructor(props: BlockNegationProps) {
+//     super(props.id, props.x, props.y);
+//     this.value = props.value;
+//   }
+// }
+
+// export class BlockLogical extends BlockBase {
+//   name: string = 'logical';
+//   type: string = 'expressionLogical';
+//   operator: string;
+//   value: BlockObject[];
+
+//   constructor(props: BlockLogicalProps) {
+//     super(props.id, props.x, props.y);
+//     this.operator = props.operator;
+//     this.value = props.value;
+//   }
+// }
--- src/constants/blockTypeObject.ts
+++ src/constants/blockTypeObject.ts
@@ -1,22 +1,29 @@
-export const BLOCK_TYPE_OBJECT = [
-  {
+interface ConstantTypeObject {
+  [key: string]: {
+    name: 'declare' | 'general' | 'control' | 'expressionValue' | 'expressionLogical';
+    korName: string;
+  };
+}
+
+export const BLOCK_TYPE_OBJECT: ConstantTypeObject = {
+  declare: {
     name: 'declare',
     korName: '선언문',
   },
-  {
+  general: {
     name: 'general',
     korName: '일반문',
   },
-  {
+  control: {
     name: 'control',
     korName: '제어문',
   },
-  {
+  expressionValue: {
     name: 'expressionValue',
     korName: '표현식 값',
   },
-  {
+  expressionLogical: {
     name: 'expressionLogical',
     korName: '표현식 논리',
   },
-];
+};
