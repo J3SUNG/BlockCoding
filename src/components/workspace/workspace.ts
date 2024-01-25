be5866813b0d140f4eb67d6f919b86191c471c54
J3SUNG <wptjd6141@naver.com>
2024-01-25 18:08:40 +0900

    FEPIT-190 : workspace 구조 변경, 기능 구현
    - 이벤트 연결, 유틸 함수 workspace/utils에서 처리
    - 워크스페이스 내에 드래그 앤 드랍 구현
    - 연결, 삭제, 이동 가능

--- src/components/workspace/workspace.ts
+++ src/components/workspace/workspace.ts
@@ -1,11 +1,9 @@
 import { UpdateWorkspaceDataAll, UpdateWorkspaceDataValue, WorkspaceData } from '../../types/stateType';
 import { createElementCommon } from '../../utils/createElementCommon';
-import { deepCopyObject } from '../../utils/deepCopyObject';
-import { createUniqueId } from '../../utils/createUniqueId';
+import { addWorkspaceMouseDragEvent } from './utils/addWorkspaceMouseDragEvent';
+import { addWorkspaceReceiveDragEvent } from './utils/addWorkspaceReceiveDragEvent';
+import { BlockObjectValue } from '../../types/blockObject';
 import { blockController } from '../../utils/blockController';
-import { BlockObject, BlockObjectValue } from '../../types/blockObject';
-import { findTargetBlock } from '../../utils/findTargetBlock';
-import { createNewBlockClass } from '../../constants/blockClass';
 
 interface WorkspaceProps {
   workspaceData: WorkspaceData;
@@ -18,111 +16,17 @@ export const workspace = ({ workspaceData, updateWorkspaceDataAll, updateWorkspa
   const trashBin = createElementCommon('div', { id: 'trash-bin' });
   const trashIcon = createElementCommon('span', { className: 'material-symbols-outlined', textContent: 'delete' });
 
-  section.addEventListener('dragover', function (event: DragEvent) {
-    event.preventDefault();
-  });
+  addWorkspaceReceiveDragEvent(section, workspaceData, updateWorkspaceDataAll);
+  addWorkspaceMouseDragEvent(section, workspaceData, updateWorkspaceDataAll);
 
-  section.addEventListener('drop', function (event: DragEvent) {
-    event.preventDefault();
-    if (event.target !== section) {
-      const target = event.target as Element;
-      const uniqueId = target.closest('div')?.id ?? '';
-      const name = event.dataTransfer!.getData('name');
-      const type = event.dataTransfer!.getData('type');
-
-      const newWorkspaceData = onDropAnotherBlock(uniqueId, name, type, workspaceData);
-      updateWorkspaceDataAll([...newWorkspaceData]);
-    } else {
-      const newWorkspaceData = onDropWorkspace(section, event, workspaceData);
-      updateWorkspaceDataAll([...newWorkspaceData]);
-    }
-  });
+  section.appendChild(trashBin);
+  trashBin.appendChild(trashIcon);
 
   workspaceData.forEach((obj) => {
     paintWorkspace(section, obj, obj.data.x, obj.data.y, 100, 50, updateWorkspaceDataValue);
   });
 
   return section;
-};
-
-const onDropWorkspace = (section: HTMLElement, event: DragEvent, workspaceData: WorkspaceData) => {
-  const newWorkspaceData = deepCopyObject(workspaceData);
-  const name = event.dataTransfer!.getData('name');
-  const offsetX = event.dataTransfer?.getData('offsetX');
-  const offsetY = event.dataTransfer?.getData('offsetY');
-  const rect = section.getBoundingClientRect();
-  const x = event.clientX - rect.left - Number(offsetX);
-  const y = event.clientY - rect.top - Number(offsetY);
-
-  const uniqueId = createUniqueId();
-  const newBlock = createNewBlockClass(name, uniqueId, x, y);
-  newWorkspaceData.push(newBlock);
-
-  return newWorkspaceData;
-};
-
-export const onDropAnotherBlock = (
-  targetUniqueId: string,
-  name: string,
-  type: string,
-  workspaceData: BlockObject[],
-) => {
-  const newWorkspaceData = deepCopyObject(workspaceData);
-  const targetObj = findTargetBlock(targetUniqueId, newWorkspaceData);
-  if (!targetObj) {
-    return;
-  }
-  const newObj: BlockObjectValue = blockOverlapEvent(targetObj, name, type);
-
-  if (newObj) {
-    const uniqueId = createUniqueId();
-
-    const newBlock = deepCopyObject(createNewBlockClass(name, uniqueId, 0, 0));
-
-    if (Array.isArray(newObj.data.value)) {
-      newObj.data.value.push(newBlock);
-    } else if (typeof newObj === 'object' && newObj !== null) {
-      newObj.data.value = newBlock;
-    }
-  }
-
-  return newWorkspaceData;
-};
-
-// TODO: 함수가 제대로 동작하지 않음 수정 필요.
-const blockOverlapEvent = (obj: BlockObject, name: string, type: string): BlockObject => {
-  const targetType = obj.type;
-  if (targetType === 'declare') {
-    if (type === 'general' || type === 'control') {
-      // TODO: 선언 블럭 안에 일반, 제어 블럭 삽입
-    }
-  } else if (targetType === 'general') {
-    if (type === 'general' || type === 'control') {
-      // TODO: 일반 블럭 위, 아래에 일반, 제어 블럭 연결
-    } else if (type === 'expressionValue' || type === 'expressionLogical') {
-      // TODO: 일반 블럭의 값에 표현식 삽입
-      if (name === 'value') {
-      }
-    }
-  } else if (obj.type === 'control') {
-    if (type === 'general' || type === 'control') {
-      // TODO: 조건 블럭 내부에 일반, 제어 블럭 삽입
-    } else if (type === 'expressionLogical') {
-      // TODO: 조건 블럭의 조건에 논리식 삽입
-    }
-  } else if (obj.type === 'expressionValue') {
-    if (type === 'expressionValue' || type === 'expressionLogical') {
-      // TODO: 값 블럭의 값에 값, 논리식 삽입
-    }
-  } else if (obj.type === 'expressionLogical') {
-    if (type === 'expressionValue' || type === 'expressionLogical') {
-      // TODO: 논리 블럭의 값에 값, 논리식 삽입
-    }
-  }
-
-  return obj;
-  // EXCEPTION : blockOverlapEvent 에러
-  // throw new Error('blockOverlapEvent 에러 - 예상치 못한 에러');
 };
 
 const paintWorkspace = (
@@ -161,6 +65,7 @@ const paintWorkspace = (
         type: obj.type,
         updateWorkspaceDataValue,
       });
+
       section.appendChild(div);
 
       paintWorkspace(section, obj.data.value, x, y + addY, width, height, updateWorkspaceDataValue);
