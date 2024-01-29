ae0510c843e73dfe05d8b048ebf12eac7de3a007
J3SUNG <wptjd6141@naver.com>
2024-01-29 12:15:42 +0900

    FEPIT-209 : 출력 블럭 클래스 메서드 추가

--- src/classes/block/blockOutput.ts
+++ src/classes/block/blockOutput.ts
@@ -1,4 +1,4 @@
-import { BlockObject } from '../../types/blockObject';
+import { BlockObject, BlockObjectValue } from '../../types/blockObject';
 import { createElementCommon } from '../../utils/createElementCommon';
 import { BlockCommon } from './blockClassCommon';
 
@@ -11,16 +11,26 @@ export class BlockOutput extends BlockCommon {
   }
 
   setChildPosition(x: number, y: number, index: number) {
-    return { childX: x, childY: 0 };
+    return { childX: x, childY: 5 };
   }
 
   paintBlock(id: string, x: number, y: number) {
     const div = createElementCommon('div', { id, className: `block block--general` });
     const p = createElementCommon('p', { className: 'block__text', textContent: '출력' });
+    const space = createElementCommon('span', { className: 'block__space' });
 
     div.setAttribute('style', `left: ${x}px; top: ${y}px;`);
     div.appendChild(p);
+    div.appendChild(space);
 
     return div;
+  }
+
+  insertBlock(obj: BlockObject, type: string) {
+    if (Object.keys(this.data.value).length === 0) {
+      if (type === 'expressionValue' || type === 'expressionLogical') {
+        this.data.value = obj;
+      }
+    }
   }
 }
