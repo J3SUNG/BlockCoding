cb8c23357edde8f7783de24073e9ace70777db5c
J3SUNG <wptjd6141@naver.com>
2024-02-01 14:23:41 +0900

    FEPIT-242 : 조건문, 반복문의 경우, y위치를 계산하는 로직 구현

--- src/classes/block/blockCondition.ts
+++ src/classes/block/blockCondition.ts
@@ -6,13 +6,15 @@ export class BlockCondition extends BlockCommon {
   name = 'condition';
   type = 'control';
   defaultWidth = 100;
+  defaultHeight = 150;
 
   constructor(id: string, x: number, y: number) {
     super(id, x, y, []);
     this.data.condition = {} as BlockObject;
   }
 
-  setChildPosition(x: number, y: number, index: number) {
+  setChildPosition(index: number) {
+    console.log('setChildPosition', index);
     return { childX: 0, childY: 50 * index };
   }
 
@@ -21,13 +23,17 @@ export class BlockCondition extends BlockCommon {
     const p = createElementCommon('p', { className: 'block__text', textContent: '조건문' });
     const space = createElementCommon('span', { className: 'block__space' });
     const childWidth = this.calcWidth();
+    const childSpace = createElementCommon('span', { className: 'block__child' });
 
-    space.setAttribute('style', `width: ${this.spaceWidth[0]}px;`);
-    div.setAttribute('style', `left: ${x}px; top: ${y}px; width: ${childWidth}px;`);
+    space.setAttribute('style', `width: ${this.spaceWidth[0]}px; margin-top: 5px;`);
+    div.setAttribute('style', `left: ${x}px; top: ${y}px; width: ${childWidth}px; height: ${this.defaultHeight}px;`);
+    p.setAttribute('style', `padding-top: 12px`);
+    childSpace.setAttribute('style', `width: ${childWidth - 50}px; height: ${this.defaultHeight - 100}px;`);
     div.appendChild(p);
     div.appendChild(space);
+    div.appendChild(childSpace);
 
-    return { block: div, space: [space, div] };
+    return { block: div, space: [space, childSpace] };
   }
 
   insert(obj: BlockObject) {
