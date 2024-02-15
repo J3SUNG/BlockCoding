fa896377c6622b641f0403db00a9a13b829c921a
J3SUNG <wptjd6141@naver.com>
2024-02-15 17:28:10 +0900

    FEPIT-337 : 객체 내부 값 private, 명확한 타입 지정

--- src/classes/exception/exception.ts
+++ src/classes/exception/exception.ts
@@ -1,57 +1,59 @@
 import { EXCEPTION } from '../../constants/exceptionMap';
 
+type ExceptionType = '' | 'infinityLoop' | 'nan' | 'infinity';
+
 export class Exception {
-  startTime: Date | null = null;
-  useTime: number = 0;
-  exception: string;
+  #startTime: Date | null = null;
+  #useTime: number = 0;
+  #exception: ExceptionType;
 
   constructor() {
-    this.startTime = new Date();
-    this.exception = '';
+    this.#startTime = new Date();
+    this.#exception = '';
   }
 
   startTimer() {
-    if (this.startTime === null) {
-      this.startTime = new Date();
+    if (this.#startTime === null) {
+      this.#startTime = new Date();
     }
   }
 
   stopTimer() {
-    if (this.startTime !== null) {
+    if (this.#startTime !== null) {
       const endTime = new Date();
-      this.useTime += endTime.getTime() - this.startTime.getTime();
-      this.startTime = null;
+      this.#useTime += endTime.getTime() - this.#startTime.getTime();
+      this.#startTime = null;
     }
   }
 
   isInfinityLoop() {
     const endTime = new Date();
-    if (this.startTime) {
-      const curUseTime = this.useTime + endTime.getTime() - this.startTime.getTime();
+    if (this.#startTime) {
+      const curUseTime = this.#useTime + endTime.getTime() - this.#startTime.getTime();
 
       if (curUseTime > 10000) {
-        this.exception = 'infinityLoop';
+        this.#exception = 'infinityLoop';
       }
     }
   }
 
   isNan(value: number) {
     if (isNaN(value)) {
-      this.exception = 'nan';
+      this.#exception = 'nan';
     }
   }
 
   isInfinity(value: number) {
     if (value === Infinity || value === -Infinity) {
-      this.exception = 'infinity';
+      this.#exception = 'infinity';
     }
   }
 
   get isError() {
-    return this.exception !== '';
+    return this.#exception !== '';
   }
 
   errorMessage() {
-    return EXCEPTION[this.exception];
+    return EXCEPTION[this.#exception];
   }
 }
